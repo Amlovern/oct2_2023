d25eb0f823de1de0f734d008f2476818a579d407
Anthony Lovern <voltorodarkwatch@gmail.com>
2024-01-11 13:52:11 -0500

    Through intermediate querying lecture

--- W10D4_notes.js
+++ W10D4_notes.js
@@ -22,6 +22,36 @@ What about deleting?
         On Delete Set Null
         Note that we cannot do this if the column has NOT NULL
 
+New Query Keywords
+    BETWEEN - inclusive
+        WHERE <column> BETWEEN <value 1> AND <value 2>
+    IN - something is true from a group of possibilities
+        WHERE <column> IN (<comma separated values>)
+    LIKE - search for partial strings
+        WHERE <column> LIKE %<partial string>%
+        % = wildcard character
+        Case insensitive in SQLite
+        Contains anywhere: '%<string>%'
+        Starts With: '<string>%'
+        Ends With: '%<string>'
+    ORDER BY - Allows us to go against the default ordering behavior
+        ORDER BY <column>
+        Default order is ASC, but we can add DESC to our command to reverse that
+        Can order by multiple factors by adding more columns separated by commas
+    LIMIT - only returns a certain number of records
+        LIMIT <value>
+    OFFSET - skip a certain number of records
+        OFFSET <value>
+        Can only be used in conjuction with LIMIT
+        Usually used in Pagination
+    DISTINCT - returns only distinct (different) values
+        SELECT DISTINCT <column> FROM <table>;
+    We can also perform mathematical operations to our columns in a SELECT
+        SELECT <column> + <value> FROM <table>;
+        This doesn't actually change the data in the DB
+    We can also alias our columns in a SELECT
+        SELECT <column> AS <alias> FROM <table>;
+
 
 
 
