5f0f3709fca7544b511b3fa5723b8632efe4ebf5
Anthony Lovern <voltorodarkwatch@gmail.com>
2024-01-16 17:20:35 -0500

    Through end of D1 and setup for D2 lecture

--- W11D2_notes.js
+++ W11D2_notes.js
@@ -1,18 +1,75 @@
 /*
 
+What makes SQL inefficient?
+    Connections to the DB
+    Large data sets
+        SQL is going to check every single line
+
+What can we do to improve efficiency?
+    Use Join and Subqueries
+        This reduces the number of DB connections
+    Indexes
+    Benchmarking
+    SELECT only the data we need
 
 
+What is the time complexity of a query without an index? And with an index?
+    Without: O(n) where n is the num of records on the table
+    With: O(log n) where n is the num of records on the table
 
+What is the syntax for creating an index?
+    CREATE INDEX <index name> ON <table name> (<list of cols>)
 
+What is the naming convention for indexes? i.e. table users, cols: first_name, last_name
+    idx_users_first_name_last_name
+    CREATE INDEX idx_users_first_name_last_name ON users (first_name, last_name)
 
+Every time we add an index, all of our other operations (CUD) become more expensive
 
+We want to make this more efficient, but before we start throwing indexes in there, we need to have a base of how long the query takes
 
+How do we start a benchmark?
+    .timer on
 
+The first time we run a query is the most expensive
+    This is due to how sqlite does caching under the hood
 
+How do we check if a query is already using an index?
+    EXPLAIN QUERY PLAN
+    <the query code>
 
+What is the difference between SCAN and SEARCH responses from EXPLAIN QUERY PLAN?
+    SCAN - checking all records
+    SEARCH - binary searching using an index
 
+What steps do we take to benchmark a query?
+    Turn .timer on
+    Run query to get initial time
+    EXPLAIN QUERY PLAN to see if an index is being used
+    If no index, add an index
+    EXPLAIN QUERY PLAN again to confirm that the index is being used
+    Run query again to get new time
 
+Every time we add a UNIQUE constraint to a column, we are adding an index
+    This allows SQL to quickly identify if the UNIQUE constraint passes or fails
 
+What is an N+1 Query?
+    When we run an initial query, then iterate over those results and for each result, we run an additional query
 
+    groups = SELECT * FROM food_groups;
+    groups.forEach(group => {
+        SELECT * FROM foods
+        WHERE food_group_id = group.id
+    })
+
+During your time here at a/A, don't worry about efficiency. Focus on getting your code to work, THEN go back and refactor.
+!! LAZY LOAD YOUR AGGREGATE DATA !!
+
+SQL Injection Attacks
+
+When a malicious users directly inputs SQL into the backend of an API
+
+Biggest takeaways so far:
+    How to benchmark queries and how to recognize N+1 queries
 
 */
---
+++ demo_code/old_stuff/app.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const actorsRouter = require('./routes/actors');
+
+app.use(express.json());
+
+// Create a middleware to print the path of the request
+// Maybe use req.url?
+const printPath = (req, res, next) => {
+    console.log(`path: ${req.url}`);
+    console.log(req.originalUrl)
+    next();
+};
+
+// Create a middleware to validate user input
+const checkUserInput = (req, res, next) => {
+    if (req.body.stuff) {
+        // console.log('inside if block')
+        return next();
+    }
+    // console.log('outside if block')
+    res.status(400).send('Please include a stuff property.')
+};
+
+// app.use((req, res, next) => {
+//     console.log('error test');
+//     const error = new Error('There was an error :(');
+//     // const error = 'There was an error :(';
+//     error.statusCode = 401;
+//     next(error);
+// })
+
+app.use('/actors', printPath);
+
+app.use('/actors', actorsRouter);
+
+app.get(['/test', '/status'], [printPath, checkUserInput], (_req, res) => {
+    res.send('Hello from your first Express API!')
+})
+
+app.get('/request', (req, res) => {
+    console.log(req)
+})
+
+app.post('/create', checkUserInput, (req, res) => {
+    res.json(req.body)
+})
+
+// app.get('/actors', (err, req, res, next) => {
+//     console.log('inside /actors endpoint')
+//     res.send('actors endpoint')
+// })
+
+// app.get('/actors/:actorId/movies/:movieId', (req, res) => {
+//     res.json(req.params)
+// })
+
+// app.get('/actors/*', (req, res) => {
+//     res.send('actors catch all')
+// })
+
+app.get('/search', (req, res) => {
+    res.json(req.query)
+})
+
+app.get('/environment', (req, res) => {
+    console.log(process.env);
+    res.json(process.env.MESSAGE);
+})
+
+// Custom 404 Error Middleware
+app.use((req, res, next) => {
+    const notFoundErr = new Error(`${req.path} not found.`);
+    notFoundErr.statusCode = 404;
+    next(notFoundErr);
+});
+
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+    console.log(err);
+    const status = err.statusCode || 500;
+    // let status;
+    // if (err.statusCode) {
+    //     status = err.statusCode
+    // } else {
+    //     status = 500
+    // }
+    res.status(status);
+    res.json({
+        message: err.message || 'Something went wrong...',
+        status
+    })
+});
+
+app.listen(8000, () => console.log('Listening on port 8000...'))
